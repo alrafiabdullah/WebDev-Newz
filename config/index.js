07fa1d1c703be3092b19f1d7fa42d0f3ea922a5d
abdullah-al-rafi <abdullah34alrafi@gmail.com>
2021-01-27 15:25:34 +0600

    domain updated

--- config/index.js
+++ config/index.js
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== "production";
 
-export const server = dev ? "http://localhost:3000" : "https://webdev-newz.vercel.com";
+export const server = dev ? "http://localhost:3000" : "https://webdev-newz.vercel.app";
