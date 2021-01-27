96a98029d97871186818ee37f533e925d9e416fd
abdullah-al-rafi <abdullah34alrafi@gmail.com>
2021-01-27 15:20:34 +0600

    initial commit

---
+++ components/ArticleItem.js
@@ -0,0 +1,16 @@
+import Link from "next/link";
+
+import articleStyle from "../styles/Article.module.css";
+
+const ArticleItem = ({ article }) => {
+    return (
+        <Link href="/article/[id]" as={`/article/${article.id}`}>
+            <a className={articleStyle.card}>
+                <h3>{article.title} &rarr;</h3>
+                <p>{article.excerpt}</p>
+            </a>
+        </Link>
+    );
+};
+
+export default ArticleItem;
---
+++ pages/about.js
@@ -0,0 +1,10 @@
+import Meta from "../components/Meta";
+
+export default function about() {
+    return (
+        <div>
+            <Meta title="About" />
+            <h3>About</h3>
+        </div>
+    );
+}
